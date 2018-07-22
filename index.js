a76f0d4b9db34705df233583bf1640fb0e8a5693
Joe Strachan <joe.strachan@ymail.com>
2018-07-22 22:29:02 +0000

    [RIDE_TO_FRONTEND] - Express / Index file serving fixed.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('build/index.html'))
+  .use(express.static(path.join(__dirname, 'build')))
+  .set('view engine', 'html')
+  .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
