8cab9911cdcec89dc3cb79afd77516103c84611e
Joe Strachan <joe.strachan@ymail.com>
2018-07-23 00:55:52 +0100

    [RIDE_TO_FRONTEND] - Account for Heroku 'App' prefix

--- server.heroku.js
+++ server.heroku.js
@@ -6,12 +6,17 @@ const cons = require('consolidate');
 express()
    .use(express.static(path.join(__dirname, 'build')))
    .get('/', function (req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+       console.log('***** GET HTML *****');
+       console.log(__dirname);
+       res.sendFile(path.join(__dirname, 'build', 'index.html'));
    })
    .get('/static/css/*', function (req, res) {
-      res.sendFile(path.join(__dirname, 'build', req.path));
+      
+     res.sendFile(path.join(__dirname, 'build', req.path));
    })
    .get('/static/js/*', function (req, res) {
-      res.sendFile(path.join(__dirname, 'build', req.path));
+     console.log('***** GET JS *****');
+
+     res.sendFile(path.join(__dirname, 'build', req.path));
    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
