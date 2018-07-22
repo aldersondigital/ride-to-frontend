e5e921f6eb93fae76acef0db201568cf3134b0bf
Joe Strachan <joe.strachan@ymail.com>
2018-07-22 23:33:31 +0100

    [RIDE_TO_FRONTEND] - Remove 'html' module reference.

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'build')))
-  .set('view engine', 'html')
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
