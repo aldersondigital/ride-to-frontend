d268471c3787041e3ed8e3bdcc282ee9a3eca96a
Joe Strachan <joe.strachan@ymail.com>
2018-07-22 23:50:44 +0100

    [RIDE_TO_FRONTEND] - Use consolidate / swig

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cons = require('consolidate');
 
 express()
+  .engine('html', cons.swig)
+  .set('view engine', 'html')
   .use(express.static(path.join(__dirname, 'build')))
-  .get('/', (req, res) => res.render('index'))
+  .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
