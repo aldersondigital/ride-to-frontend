3a72574c081fb347aaa318fdc46a0d329054e705
Joe Strachan <joe.strachan@ymail.com>
2018-07-23 00:16:55 +0100

    [RIDE_TO_FRONTEND] - Fix Express routing for Heroku

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 5000
 const cons = require('consolidate');
 
 express()
-  .engine('html', cons.swig)
-  .set('view engine', 'html')
-  .use(express.static(path.join(__dirname, 'build')))
-  .get('/', (req, res) => res.render('index.html'))
+   .use(express.static(path.join(__dirname, 'build')))
+   .get('/', function (req, res) {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
