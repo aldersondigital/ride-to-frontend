8df865a30128069feda142d64108a1c39d50c8db
Joe Strachan <joe.strachan@ymail.com>
2018-07-23 00:43:24 +0100

    [RIDE_TO_FRONTEND] - Alter server.heroku.js

--- server.dev.js
+++ server.dev.js
@@ -8,4 +8,10 @@ express()
    .get('/', function (req, res) {
       res.sendFile(path.join(__dirname, 'app', 'build', 'index.html'));
    })
+   .get('/static/css/*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'app', 'build', req.path));
+   })
+   .get('/static/js/*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'app', 'build', req.path));
+   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ server.heroku.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const cons = require('consolidate');
+
+express()
+   .use(express.static(path.join(__dirname, 'build')))
+   .get('/', function (req, res) {
+      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+   })
+   .get('/static/css/*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'build', req.path));
+   })
+   .get('/static/js/*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'build', req.path));
+   })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
